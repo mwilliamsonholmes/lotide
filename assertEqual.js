5bd27d8db50d3a099a52084b2885cb76234948ef
Melissa <mawilliamson10@gmail.com>
2022-01-11 04:02:58 +0000

    created assertEqual function)
    eslint assertEqual.js
    cd lotide

---
+++ assertEqual.js
@@ -0,0 +1,11 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log("Assertion Passed: " + actual + " === " + expected);
+  } else {
+    console.log("Assertion Failed: " + actual + " !== " + expected);
+  }
+};
+assertEqual("Lighthouse Labs", "Hi");
+assertEqual(1, 5);
+
+
