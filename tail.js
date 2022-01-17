9d677a1100f38541c7cc416ca3cb0b2c7a625d42
Melissa <mawilliamson10@gmail.com>
2022-01-17 21:08:13 +0000

    modularizing tail function

--- tail.js
+++ tail.js
@@ -1,16 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
 
 const tail = function(array) {
   let answer = array.slice(1);
   return answer;
 };
-
-const words = ["hello"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+module.exports = tail;
