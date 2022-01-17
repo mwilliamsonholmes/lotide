7bb563b20ad27ed92423cd86aeca6166c43c2dbe
Melissa <mawilliamson10@gmail.com>
2022-01-17 21:06:36 +0000

    test file that requires the head function

---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const tail = require('../tail');
+const assertEqual = require('../assertEqual');
+
+const result = tail(["Hello", "Lighthouse", "Labs"]);
+assertEqual(result.length, 2); // ensure we get back two elements
+assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
+assertEqual(result[1], "Labs");
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3);
