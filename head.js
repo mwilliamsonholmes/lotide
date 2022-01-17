c5f66fc179d675d3022d0efc8e971633ff582c36
Melissa <mawilliamson10@gmail.com>
2022-01-17 17:55:53 +0000

    modularizing head function

--- head.js
+++ head.js
@@ -1,13 +1,6 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
-const head = function(array) {
+const head = function (array) {
   return array[0];
 }
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;
