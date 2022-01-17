9bcd04399aabb5a9895a16108bc35e9e66eb4a3f
Melissa <mawilliamson10@gmail.com>
2022-01-17 17:54:59 +0000

    modularizing assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,12 +1,12 @@
-const assertEqual = function(actual, expected) {
+const assertEqual = function (actual, expected) {
   if (actual === expected) {
-    console.log("Assertion Passed: " + actual + " === " + expected);
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
-    console.log("Assertion Failed: " + actual + " !== " + expected);
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
   }
 };
-assertEqual("Lighthouse Labs", "Hi");
-assertEqual(1, 5);
-//checking
+
+module.exports = assertEqual;
+
 
 
