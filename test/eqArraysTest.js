04a8e7be550239fd48e04cdecfbd589db02712b6
Melissa <mawilliamson10@gmail.com>
2022-01-17 21:52:59 +0000

    tests that require the eqArrays function

---
+++ test/eqArraysTest.js
@@ -0,0 +1,9 @@
+const eqArrays = require('../eqArrays');
+const assertEqual = require('../assertEqual');
+
+console.log(eqArrays([1, 2, 3], [1, 2, "3"]));
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); //true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); //false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true) // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true) // => false
