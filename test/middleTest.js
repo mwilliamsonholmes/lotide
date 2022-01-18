f858cd764fbc54f5d3cb856e2f1031c530dee5b9
Melissa <mawilliamson10@gmail.com>
2022-01-18 00:51:04 +0000

    uses Chai

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,4 +1,15 @@
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
+const assert = require('chai').assert;
+
+describe("#middle", () => {
+  it("returns [2, 3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+});
+
+
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]);
