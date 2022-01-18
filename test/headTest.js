5fe7f4b8d969236e3eeec2b9ac63489956990d17
Melissa <mawilliamson10@gmail.com>
2022-01-18 00:01:16 +0000

    used Chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,4 +1,12 @@
 const head = require('../head');
-const assertEqual = require('../assertEqual');
-assertEqual(head([5, 6, 7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hi");
+const assert = require('chai').assert;
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+});
+
